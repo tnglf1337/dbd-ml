7a796b7f9491a96b058dc653f10798d43bd8d041
Timo Neske <Timo.Neske@hhu.de>
2025-07-27 23:19:06 +0200

    update project

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 from extractor import read_screenshot, crop_rotated_rect, clean
 from csv_maker import create_csv_from_perks
 
-create_csv_from_perks("dpd-killer-perks/png", "killer_perks.csv")
+#create_csv_from_perks("dpd-killer-perks/png", "killer_perks.csv")
+
+
 
 
 
